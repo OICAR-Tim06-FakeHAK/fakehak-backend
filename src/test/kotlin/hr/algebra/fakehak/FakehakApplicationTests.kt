9f87eb85c8db68e16a4d8dc71b4b4dee79ea9817
sukalov <sukalov@algebra.hr>
2026-03-25 18:56:08 +0100

    create initial project for backend

---
+++ src/test/kotlin/hr/algebra/fakehak/FakehakApplicationTests.kt
@@ -0,0 +1,13 @@
+package hr.algebra.fakehak
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FakehakApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
