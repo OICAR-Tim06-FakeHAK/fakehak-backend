0bedcdb9e4adcc3ccaa0e87083a9e16841030148
boris <bmestric@algebra.hr>
2026-04-09 13:20:49 +0200

    another fix to for login and no more deleting db

--- src/main/kotlin/hr/algebra/fakehak/controller/AuthController.kt
+++ src/main/kotlin/hr/algebra/fakehak/controller/AuthController.kt
@@ -2,8 +2,10 @@ package hr.algebra.fakehak.controller
 
 import hr.algebra.fakehak.dto.login.LoginRequestDto
 import hr.algebra.fakehak.dto.login.LoginResponseDto
+import hr.algebra.fakehak.exception.InvalidOperationException
 import hr.algebra.fakehak.service.AuthService
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -16,6 +18,10 @@ class AuthController(private val authService: AuthService) {
 
     @PostMapping("/login")
     fun login(@Valid @RequestBody request: LoginRequestDto): ResponseEntity<LoginResponseDto> {
-        return ResponseEntity.ok(authService.login(request))
+        return try {
+            ResponseEntity.ok(authService.login(request))
+        } catch (e: InvalidOperationException) {
+            ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
+        }
     }
 }
