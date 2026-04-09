1ff4d656bc4b34e6901ec22521da3933a6336934
sukalov <sukalov@algebra.hr>
2026-04-09 15:34:58 +0200

    added explicit allow for post to login, commented cors

--- src/main/kotlin/hr/algebra/fakehak/config/CorsConfig.kt
+++ src/main/kotlin/hr/algebra/fakehak/config/CorsConfig.kt
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+/*
+
+Privremeno, barem zasada - sukalov
+
 @Configuration
 class CorsConfig : WebMvcConfigurer {
-
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
@@ -16,3 +19,4 @@ class CorsConfig : WebMvcConfigurer {
             .maxAge(3600)
     }
 }
+ */
